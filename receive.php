2dbc8aa756adb5611217c3b31c4df0d3796a6055
Norn <leave.orz@gmail.com>
2017-11-07 23:50:06 +0900

    不要ファイルを削除

--- receive.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-$json_string = file_get_contents('php://input'); ##今回のキモ
-echo $json_string;
-$obj = json_decode($json_string);
-var_dump($obj);
-?>
